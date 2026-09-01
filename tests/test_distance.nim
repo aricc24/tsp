756de45d5a864e01f3f2113f63b39af6c1173e58
aricc24 <aricc24@ciencias.unam.mx>
2026-08-31 19:17:15 -0600

    test: natural distance for a quarter of the equator

--- tests/test_distance.nim
+++ tests/test_distance.nim
@@ -2,7 +2,7 @@ import unittest
 import ../src/models/city
 import ../src/tsp/distance
 #import ../src/persistence/database
-
+import std/math
 
 const Epsilon = 1e-7
 
@@ -21,7 +21,27 @@ suite "Natural Distance":
     let result = naturalDistance(city, city)
 
     check abs(result) <= Epsilon
-
   
+  test "Natural distance for a quarter of the equator":
+    let cityA = City(
+      id: 1, 
+      name: "A",
+      country: "X",
+      population: 0,
+      latitude: 0.0,
+      longitude: 0.0
+    )
 
+    let cityB = City( 
+      id: 2,
+      name: "B",
+      country: "X",
+      population: 0,
+      latitude: 0.0,
+      longitude: 90.0
+    )
 
+    let result = naturalDistance(cityA, cityB)
+    let expected = 6_373_000.0 * PI / 2.0
+
+    check abs(result - expected) <= Epsilon
