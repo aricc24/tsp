5d321ac55481d72615698054d66182414af22993
aricc24 <aricc24@ciencias.unam.mx>
2026-09-03 17:03:17 -0600

    fear: add neighbor function

--- src/heuristics/threshold_acceptance/neighbor.nim
+++ src/heuristics/threshold_acceptance/neighbor.nim
@@ -1,4 +1,19 @@
-
+import std/random
 
 proc swapPositions*[T](solution: var seq[T], i:int, j:int) = 
     swap(solution[i], solution[j])
+
+proc neighbor*[T](solution: var seq[T], rng: var Rand): tuple[i: int, j: int] =
+
+    if solution.len < 2: 
+        raise newException(ValueError, "Two elements are neded")
+
+    let i = rng.rand(solution.high)
+    var j = rng.rand(solution.high - 1)
+
+    if j >= i: 
+        inc j
+
+    swapPositions(solution, i, j)
+
+    return(i, j)
