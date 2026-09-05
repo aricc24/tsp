876b78e7946a54bc31cc7f4a7fb7028e45cd83fd
aricc24 <aricc24@ciencias.unam.mx>
2026-09-05 16:51:07 -0600

    feat: frist test of components working thogether

--- src/main.nim
+++ src/main.nim
@@ -1,6 +1,15 @@
 import parseopt
-import strutils
 import random
+import ./models/city
+import ./persistence/database
+import ./tsp/graph
+import ./tsp/weights
+import ./tsp/cost
+import ./tsp/feasibility
+import ./heuristics/threshold_acceptance/config
+import ./heuristics/threshold_acceptance/threshold_acceptance
+import ./persistence/instance_file
+
 
 type CliOptions = object
     databasePath: string
@@ -42,3 +51,45 @@ proc validateArguments(options: CliOptions) =
             ValueError,
             "Missing required option: --instance"
         )
+
+proc main() =
+    let options = parseArguments()
+    validateArguments(options)
+
+    let cities = getCities(options.databasePath)
+    let connections = getConnections(options.databasePath)
+
+    let graph = buildGraph(cities, connections)
+    var solution = loadInstance(options.instancePath, cities)
+    let maxDist = maximumDistance(solution, connections)
+    let norm = normalizer(solution,graph)
+
+    proc tspCost(path: seq[City]): float =
+        cost(path, graph, maxDist, norm)
+
+    let config = ThresholdConfig(
+        initialTemperature: 1.0,
+        epsilon: 0.01,
+        coolingFactor: 0.9,
+        batchSize: solution.len,
+        maxAttempts: 10 * solution.len,
+        maxBatchesPerTemperature: 20
+    )
+
+    var rng = initRand(123)
+
+    let initialCost = tspCost(solution)
+
+    let result = thresholdAcceptance(solution,config,rng,tspCost)
+
+    echo "Instance: ", options.instancePath
+    echo "Initial cost: ", initialCost
+    echo "Best cost: ", result.bestCost
+    echo "Feasible: ",
+        if isFeasible(result.bestSolution, graph):
+            "YES"
+        else:
+            "NO"
+        
+when isMainModule:
+    main()
