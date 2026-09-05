4cff2fab8d89948a7f6aea54ea79eba3f6df7917
aricc24 <aricc24@ciencias.unam.mx>
2026-09-05 14:43:27 -0600

    feat: isFeasible returns false when a consecutive connection is missing

--- tests/test_feasibility.nim
+++ tests/test_feasibility.nim
@@ -22,3 +22,20 @@ suite "Feasibility":
         ]
 
         check isFeasible(path, graph) 
+    
+    test "Returns false when a consecutive connection is missing":
+        let graph = Graph(
+            adjacencyMatrix: @[
+                @[0.0, 100.0, 0.0],
+                @[100.0, 0.0, 0.0],
+                @[0.0, 0.0, 0.0]
+            ]
+        )
+
+        let path = @[
+            City(id: 1),
+            City(id: 2),
+            City(id: 3)
+        ]
+
+        check not isFeasible(path, graph)
