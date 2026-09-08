997c07985f10e735b7b00a4f10454ff056567349
aricc24 <aricc24@ciencias.unam.mx>
2026-09-07 19:02:58 -0600

    feat: add runner for threshold acceptance

---
+++ src/runner/runner.nim
@@ -0,0 +1,34 @@
+import std/random
+import ../heuristics/threshold_acceptance/config
+import ../heuristics/threshold_acceptance/threshold_acceptance
+
+type 
+    RunResult*[T] = object
+        bestSolution*: seq[T]
+        bestCost*: float
+        bestSeed* int
+
+proc runMultiple*[T](initialSolution: seq[T],heuristicConfig: ThresholdConfig, runs: int, baseSeed: int,
+    costFunction: proc(solution: seq[T]): float): RunResult[T] =
+
+    var globalBestSolution = initialSolution[0 .. ^1]
+    var globalBestCost = costFunction(initialSolution)
+    var globalBestSeed = baseSeed
+
+    for runIndex in 0 ..< runs:
+        let seed = baseSeed + runIndex
+
+        var solution = initialSolution[0 .. ^1]
+        var rng = initRand(seed)
+
+        let runResult = thresholdAcceptance(solution, heuristicConfig, rng, costFunction)
+
+        if runResult.bestCost < globalBestCost:
+            globalBestCost = runResult.bestCost
+            globalBestSolution = runResult.bestSolution[0 .. ^1]
+            globalBestSeed = seed
+
+    return RunResult[T](bestSolution: globalBestSolution,
+                        bestCost: globalBestCost,
+                        bestSeed: globalBestSeed
+                    )
