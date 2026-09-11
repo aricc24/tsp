10f7af647f2aec8d16305d0692f745183863e0e0
aricc24 <aricc24@ciencias.unam.mx>
2026-09-11 16:46:14 -0600

    feat: functions for cost function optimization

--- src/tsp/cost.nim
+++ src/tsp/cost.nim
@@ -2,6 +2,24 @@ import ../models/city
 import ../models/graph
 import ./weights
 
+proc edgeCost(u: City, v: City, graph: Graph, maxDist: float, norm: float): float =
+    augmentedWeight(u, v, graph, maxDist) / norm
+
+
+proc affectedEdges*(pathLen: int, i: int, j: int): seq[int] =
+    var edges: seq[int] = @[]
+
+    proc addEdge(index: int) =
+        if index >= 0 and index < pathLen - 1 and index notin edges:
+            edges.add(index)
+
+    addEdge(i - 1)
+    addEdge(i)
+    addEdge(j - 1)
+    addEdge(j)
+
+    return edges
+
 proc cost*(path: seq[City], graph: Graph, maxDist: float, norm: float): float =
     var cost = 0.0
 
@@ -9,6 +27,6 @@ proc cost*(path: seq[City], graph: Graph, maxDist: float, norm: float): float =
         let u = path[i - 1]
         let v = path[i]
 
-        cost += augmentedWeight(u, v, graph, maxDist)
+        cost += edgeCost(u, v, graph, maxDist, norm)
 
-    return cost / norm
+    return cost
